1f0d75b4ad943e1edf621556db895139e31d2f91
SollyBunny <sollybunn@gmail.com>
2022-09-04 23:15:27 +0000

    init

---
+++ include.h
@@ -0,0 +1,62 @@
+
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <gmp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum { 
+	NUL,
+	OPERATOR, NUMBER, VARIABLE,
+	ADD, SUB, MUL, DIV, MOD, EXP,
+};
+enum {
+	B2  = 2,
+	B8  = 8,
+	B16 = 16,
+};
+
+struct Token {
+	struct Token *last;
+	struct Token *next;
+	unsigned int type;
+	union {
+		unsigned int operator;
+		mpf_t        number;
+		long         variable;
+	} v;
+};
+typedef struct Token Token;
+
+typedef struct {
+	unsigned int used;
+	long hash;
+	unsigned int type;
+	union {
+		mpf_t number;
+	} v;
+} Variable;
+Variable *variables;
+
+extern void __attribute__((noreturn)) die(const char msg[]);
+#define ADDHASH(v, t) (v) = (t) + ((v) << 6) + ((v) << 16) - (v)
+extern long hash(const char *str);
+
+extern Variable *vars;
+extern void varfree(unsigned int i);
+extern int  varadd(const char *str, unsigned int type, void *value);
+extern void varsetup();
+
+void tokenprint(Token *t);
+void tokenfree(Token *t);
+void tokenadd(Token **t, unsigned int type, void *arg1, void *arg2);
+void tokenize(unsigned int depth, char *string, Token **t);
+void tokenparse(Token *t);
+
+#include "config.h"
+#include "var.h"
+#include "token.h"
+
+#endif
---
+++ main.c
@@ -0,0 +1,62 @@
+#include "include.h"
+
+void __attribute__((noreturn)) die(const char msg[]) {
+	printf("Error: %s\n", msg);
+	exit(1);
+}
+
+long hash(const char *str) {
+	long out = 0;
+	while (*str != '\0') {
+		ADDHASH(out, *str);
+		++str;
+	}
+	return out;
+}
+
+int main(int argc, char *argv[]) {
+	// check args
+		if (argc == 1) {
+			printf("Usage: %s [EXPRESSION]\n", argv[0]);
+			exit(0);
+		}
+	// stitch strings together
+		char *string;
+		size_t size = 1;
+		for (int i = 1; i < argc; ++i) {
+			unsigned int m = 0;
+			while (argv[i][m] != '\0') ++m;
+			size += m;
+		}
+		string = malloc(size);
+		for (int i = 1; i < argc; ++i) {
+			unsigned int m = 0;
+			while (argv[i][m] != '\0') {
+				*string = argv[i][m];
+				++string;
+				++m;
+			};
+		}
+		*string = '\0';
+		string -= size - 1;
+		printf("'%s' (%lu)\n", string, size);
+	// setup
+		mpf_set_default_prec(PRECISION);
+		varsetup();
+	// tokenize
+		Token *t = NULL;
+		tokenize(0, string, &t);
+		free(string);
+		tokenprint(t);
+	// free stuff
+		Token *tnew;
+		do {
+			tnew = t->last;
+			tokenfree(t);
+			t = tnew;
+		} while (t != NULL);
+		for (unsigned int i = 0; i < varalloc; ++i) varfree(i);
+		free(vars);
+	return 0;	
+
+}
